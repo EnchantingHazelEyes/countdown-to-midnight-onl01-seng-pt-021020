a49b24725f882bc8656041190f7f84d04c75d796
Samantha Barros <samantha.barros95@gmail.com>
2020-02-16 23:56:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
+seconds = 10
 
-def countdown
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)"
+    seconds -= 1
+  until seconds == 0
+    return "HAPPY NEW YEAR!"
+  end
+  end
+  
 end
