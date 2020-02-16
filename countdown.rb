49a2af90577b6bf5e804b06f93d840e9ce9a0a06
Samantha Barros <samantha.barros95@gmail.com>
2020-02-16 23:57:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@ seconds = 10
 
 def countdown(seconds)
   while seconds > 0
-    puts "#{seconds} SECOND(S)"
+    puts " #{seconds} SECOND(S)"
     seconds -= 1
   until seconds == 0
     return "HAPPY NEW YEAR!"
